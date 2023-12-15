955f17f958ee85aa3315b4e8e9197aaa1a5fa2a2
jade <jadehh@live.com>
2023-12-15 11:28:33 +0800

    * 更新test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@
 # @Email    : jadehh@1ive.com
 # @Software : Samples
 # @Desc     :
+import json
 import os
 
 import requests
@@ -30,7 +31,7 @@ def test_spider(Spider):
     spider = Spider()
     spider.init()
     #content_list = spider.homeContent(True)["list"]
-    #content_list = spider.searchContent("奥本海默")["list"]
+    # content_list = spider.searchContent("奥本海默")["list"]
     content_list = spider.categoryContent("1",1,None,None)["list"]
     for content in [content_list[0]]:
         vod_url_list = spider.detailContent([content['vod_id']])['list'][0]['vod_play_url'].split("$$$")[0].split("#")
@@ -82,5 +83,32 @@ def test_test():
     test_spider(Spider)
 
 
+def test_re():
+    import re
+    share_url_list = ["https://www.alipan.com/s/gS9SkxCHki9",
+                      "https://www.alipan.com/s/gS9SkxCHki9/folder/657546c6661ec8b988924dc1945756a1f5e95e27"]
+    for share_url in share_url_list:
+        if "www.aliyundrive.com" in share_url:
+            m = re.search('www.aliyundrive.com\\/s\\/([^\\/]+)(\\/folder\\/([^\\/]+))?',
+                          share_url).groups()
+        elif "www.alipan.com" in share_url:
+            re_str = "https://www.alipan.com\\/s\\/([^\\/]+)(\\/folder\\/([^\\/]+))?"
+            m = re.search(re_str, share_url).groups()
+        else:
+            print("不支持的URL")
+        print(m[0])
+
+def test_logging():
+    from jade import JadeLogging
+    JadeLog = JadeLogging()
+    JadeLog.INFO("第一行输出")
+    JadeLog.ERROR("第二行输出")
+
+def test_js_log_to_txt():
+    with open("tmp/js_log","rb") as f:
+        js_log_dic = json.load(f)
+    with open("tmp/log.txt","wb") as f:
+        for value in js_log_dic.values():
+            f.write((value + "\n").encode("utf-8"))
 if __name__ == '__main__':
-    test_wanou()
+    test_js_log_to_txt()
