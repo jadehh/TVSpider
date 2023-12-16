b5df75c82d602b5e83e5f8d012c04e5e2faa8c56
jadehh <jadehh@live.com>
2023-12-16 17:04:47 +0800

    * 支持TV首页内容解析

--- js/wogg.js
+++ js/wogg.js
@@ -102,7 +102,7 @@ async function home(filter) {
     let vod_list = parseVodListFromDoc($)
     let result = JSON.stringify({
         class: classes,
-        vod_list: vod_list,
+        list: vod_list,
         filters: JSON.parse(content),
 
     });
