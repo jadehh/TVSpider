ca6ed66de746d07ac9e23de2abc3229f1a1b538e
jade <jadehh@live.com>
2024-01-04 15:29:02 +0800

    * 返回Spider对象

--- js/jiujiuliu.js
+++ js/jiujiuliu.js
@@ -9,6 +9,7 @@
 
 import {Spider} from "./spider.js";
 
+
 class JiuJiuLiuSpider extends Spider {
     constructor() {
         super();
@@ -1984,6 +1985,50 @@ class JiuJiuLiuSpider extends Spider {
     }
 }
 
+let spider = new JiuJiuLiuSpider()
+
+async function init(cfg) {
+    await spider.init(cfg)
+}
+
+
+async function home(filter) {
+    return await spider.home(filter)
+}
+
+
+async function homeVod() {
+    return await spider.homeVod()
+}
+
+
+async function category(tid, pg, filter, extend) {
+    return await spider.category(tid,pg,filter,extend)
+}
+
+
+async function detail(id) {
+    return await  spider.detail(id)
+}
+
+async function play(flag, id, flags) {
+    return await spider.play(flag,id,flags)
+}
+
+
+async function search(wd, quick) {
+   return await spider.search(wd,quick)
+}
+
+
 export function __jsEvalReturn() {
-    return new JiuJiuLiuSpider();
+    return {
+        init: init,
+        home: home,
+        homeVod: homeVod,
+        category: category,
+        detail: detail,
+        play: play,
+        search: search,
+    };
 }
