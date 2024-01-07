5eb659dfe9dc5bad61b7fa0978953ba3af86cc36
jadehh <jadehh@live.com>
2024-01-07 14:20:48 +0800

    * 字母链接放在最后

--- js/testSpider.js
+++ js/testSpider.js
@@ -70,6 +70,7 @@ async function test() {
                             var name = urls[j].split('$')[0];
                             var url = urls[j].split('$')[1];
                             console.debug(flag + " | " + name + " | " + url);
+                            url = "654c8d27f1265738cb41441b898f40c8628377ef+Oppenheimer.2023.1080p.BluRay.DD5.1.x264-GalaxyRG.chs.mkv@@@srt@@@654c46d7096533e59ea84d6082359478ffa5969c+Oppenheimer.2023.BluRay.1080p.DTS-HD.MA.5.1.AVC.REMUX-FraMeSToR.chs@@@srt@@@654c3efd27f8e9e233064fe49da8d81ca5d3fa5c+奥本海默 (2023) - Oppenheimer.2023.UHD.BluRay.2160p.DTS-HD.MA.5.1.HEVC.REMUX-FraMeSToR.chs&eng@@@ass@@@6551be3051ca57b8dd2b4f41a38f7efe39329ada+UNdNKT3Tu3Q+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXN0b21Kc29uIjoie1wiZG9tYWluX2lkXCI6XCJiajI5XCIsXCJzaGFyZV9pZFwiOlwiVU5kTktUM1R1M1FcIixcImNyZWF0b3JcIjpcImUwYzhlZTZhYWYwZjQ5YzU5MDZhYWVmZWUzM2IzMDk2XCIsXCJ1c2VyX2lkXCI6XCJhbm9ueW1vdXNcIn0iLCJjdXN0b21UeXBlIjoic2hhcmVfbGluayIsImV4cCI6MTcwNDYxNTU0NiwiaWF0IjoxNzA0NjA4Mjg2fQ.l1S4u4-LRQXDzK1hqvplI3tgAnbU7-_SPTcfgKaKFGCNK7wj0CMAkgHZBlzuUjikwMKpv2AN2SC2jBXhcdywFEgIfj_4HNC85uFdCJHIQGvpESwebtBujbAXhAyiZXyEZdfAASd2qnt92z1GI8GdNiCzNoJYQsFLsXBQSrnAtog"
                             var playUrl = await spider.play("标清", url, []);
                             console.debug('playURL: ' + playUrl);
                             break
