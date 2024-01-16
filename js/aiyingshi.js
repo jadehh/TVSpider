768f521af24df6c1881fb069807023bd81841b5c
jadehh <jadehh@live.com>
2024-01-17 00:03:08 +0800

    * 新增爱影视爬虫播放链接获取

--- js/aiyingshi.js
+++ js/aiyingshi.js
@@ -269,7 +269,6 @@ class AiYingShiSpider extends Spider {
 
     async setPlay(flag, id, flags) {
         let html = await this.fetch(this.siteUrl + id,null,this.getHeader())
-        await this.jadeLog.debug(html)
         if (!_.isEmpty(html)){
             let player_str = Utils.getStrByRegex(/<script type="text\/javascript">var player_aaaa=(.*?)<\/script>/,html)
             let play_dic = JSON.parse(player_str)
