e2b9f166f55a7b95ed8a828fca087a6c352c4605
jadehh <jadehh@live.com>
2024-01-18 22:35:10 +0800

    * 播放使用头

--- js/spider.js
+++ js/spider.js
@@ -183,7 +183,7 @@ class Spider {
         this.page = 0
         this.vodDetail = new VodDetail()
         this.playUrl = ""
-        this.header = {}
+        this.header = {"User-Agent": Utils.CHROME}
     }
 
     async reconnnect(reqUrl, params, headers, redirect_url) {
