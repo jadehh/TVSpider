0b1f964c33a7d4769cd325c0c3191290f69bcb00
jade <jadehh@live.com>
2024-01-25 17:26:16 +0800

    * 无法解决gb2312的bug

--- js/dytt.js
+++ js/dytt.js
@@ -27,15 +27,16 @@ class DyttSpider extends Spider {
         return "|电影天堂|"
     }
 
-    async getHtml(url = this.siteUrl, headers = this.getHeader()) {
-        let buffer = await this.fetch(url,null,headers,false,false,1)
-        let html = Utils.decode(buffer,"gb2312")
-        if (!_.isEmpty(html)) {
-            return load(html)
-        } else {
-            await this.jadeLog.error(`html获取失败`, true)
-        }
-    }
+    // async getHtml(url = this.siteUrl, headers = this.getHeader()) {
+    //     let buffer = await this.fetch(url,null,headers,false,false,1)
+    //
+    //     let html = Utils.decode(buffer,"gb2312")
+    //     if (!_.isEmpty(html)) {
+    //         return load(html)
+    //     } else {
+    //         await this.jadeLog.error(`html获取失败`, true)
+    //     }
+    // }
 
 
     async setFilterObj() {
