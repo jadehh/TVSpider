64d9b6f9a2652573562e2308ab5f81740eed32b4
jade <jadehh@live.com>
2024-02-05 17:09:21 +0800

    * 更新线路名称

--- js/pipixia.js
+++ js/pipixia.js
@@ -375,14 +375,14 @@ class PiPiXiaSpider extends Spider {
         let ConFig = JSON.parse(Utils.getStrByRegex(/let ConFig = (.*?),box = /, jiexi$.html()))
         let playUrl = this.uic(ConFig["url"], ConFig.config.uid)
         await this.jadeLog.debug(`播放链接为:${playUrl}`)
-        if (flag.indexOf("极速")) {
+        if (flag.indexOf("极速") > -1) {
             this.playUrl = playUrl
         } else {
             this.playUrl = this.videoProxy + Utils.base64Encode(playUrl)
         }
     }
 
-    async setSearch(wd, quick) {
+    async setSearch(wd, quick) {+
 
         let $ = await this.getHtml(this.siteUrl + `/vodsearch.html?wd=${decodeURI(wd)}`)
         this.vodList = await this.parseVodShortListFromDocBySearch($)
