c6f40b8d44ffeb40bcbe4bd7060af6677a77e68e
jade <jadehh@live.com>
2024-02-07 09:30:07 +0800

    * 更新非凡Spider

--- js/vodSpider.js
+++ js/vodSpider.js
@@ -61,7 +61,7 @@ class VodSpider extends Spider {
         vodDetail.vod_year = vod_data["vod_year"]
         vodDetail.vod_actor = vod_data["vod_actor"]
         vodDetail.vod_director = vod_data["vod_director"]
-        vodDetail.vod_content = vod_data["vod_content"].replaceAll("<p>","").replaceAll("</p>","")
+        vodDetail.vod_content = vod_data["vod_content"]
         vodDetail.vod_play_from = vod_data["vod_play_from"]
         vodDetail.vod_play_url = vod_data["vod_play_url"]
         vodDetail.type_name = vod_data["type_name"]
