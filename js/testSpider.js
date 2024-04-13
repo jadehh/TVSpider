02f3da9c9c4e3dad9473604df2dcccfe94dfa59f
jadehh <jadehh@live.com>
2024-04-13 18:08:38 +0800

    * 完成磁力熊的开发

--- js/testSpider.js
+++ js/testSpider.js
@@ -1,4 +1,4 @@
-import {__jsEvalReturn} from './jable.js';
+import {__jsEvalReturn} from './cilixiong.js';
 import * as Utils from "../lib/utils.js";
 
 
@@ -50,12 +50,12 @@ async function testMusicPlay(vodDetail) {
 }
 
 async function test() {
-    let siteKey = 'bilibili';
+    let siteKey = 'cilixiong';
     let siteType = 0;
     await spider.init({
         skey: siteKey, stype: siteType, ext: {
             "token": "6827db23e5474d02a07fd7431d3d5a5a",
-            "box": "TV",
+            "box": "CatOpen",
             "code": "1",
             "from": "justlive1",
             "danmu": true,
@@ -68,26 +68,30 @@ async function test() {
     console.debug(JSON.stringify(classes))
 
 
+
+
+    // 测试搜索
+    let search_page = JSON.parse(await spider.search("速度与激情"))
+    console.debug(JSON.stringify(search_page));
+
+
     // //测试首页列表
     let homeVod = JSON.parse(await spider.homeVod())
     console.debug(JSON.stringify(homeVod));
 
-
-
-    //测试分类列表
-    let catePage = JSON.parse(await spider.category("历史记录", "1", undefined, {}));
-    console.debug(JSON.stringify(catePage));
-
     // 测试详情
-    let detail1 = JSON.parse(await spider.detail("BV12r421t7nu"))
+    let detail1 = JSON.parse(await spider.detail("/movie/3966.html"))
     await testPlay(detail1)
 
+    //测试分类列表
+    let catePage = JSON.parse(await spider.category("/movie/", "1", undefined, {}));
+    console.debug(JSON.stringify(catePage));
 
 
 
-    // 测试搜索
-    let search_page = JSON.parse(await spider.search("海贼王"))
-    console.debug(JSON.stringify(search_page));
+
+
+
 
 
     // 测试详情
