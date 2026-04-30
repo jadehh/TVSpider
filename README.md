753e9bf644e82b30ac897d502a11bc698345f3ea
Shuyu-Zhang <624269674@qq.com>
2026-04-30 02:18:27 +0000

    * 更新TVConfig和OpenConfig

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-# tvbox_source
+# CatVodOpen和TvBox
+
+## 食用前阅读
+&ensp;&ensp;授人以鱼不如授人以渔，本仓意指在教会大家学会Spider，也欢迎大家提ISSUE，一起学习一起进步。<br>
+&ensp;&ensp;以下资源不能保证内容的合法性、准确性、完整性和有效性，请根据情况自行判断。 
+仅用于测试和学习研究，禁止用于商业用途，不得将其用于违反国家、地区、组织等的法律法规或相关规定的其他用途。
+使用任何第三方硬件、软件、所造成的一切后果由使用的个人或组织承担，与本文内容无关。
+所有直接或间接使用本内容的个人和组织，应 24 小时内完成学习和研究，并及时删除本文内容。
+所有基于本内容的源代码，进行的任何修改，为其他个人或组织的自发行为，与本内容没有任何直接或间接的关系，所造成的一切后果亦与本内容和本管理者无关。
+本管理者保留随时更改或补充此免责声明的权利。一旦使用、复制、转载、发布、修改了本文内容，则视为您已接受此免责声明。否则后果自负。
+本接口无公众号、无盈利、无引流、免费分享给小白使用，请勿轻信他人，谨防上当受骗。
+
+[使用教程见Wiki](https://github.com/jadehh/TVSpider/wiki)
+
+## TVBox互联网发布地址
+[TV发布地址](https://github.com/FongMi/Release/tree/main/apk/release)
+```bash
+https://gh.con.sh/https://raw.githubusercontent.com/jadehh/TVSpider/js/tv_config.json
+```
+> 配置信息见js分支分支
+
+
+## CatVodOpen
+[CatVodOpen发布地址](https://github.com/catvod/CatVodOpen/releases)
+
+> 注意使用Gitee或Github导入，并设置为私有仓库，<font color="red">CatVodOpen仅支持私有仓库</font>
+
+V1.1.3版本以上
+```bash
+gitee://Token@gitee.com/jadehh_743/TVSpider/dist/index.js.md5
+```
+<font color="red">**改动**</font>
+
+* quickjs改为nodejs，proxy设置修改
+* 在ios上无法使用local，使用db替换local所有方法
+* nodejs 的优势在于更加灵活
+> 配置信息见dist分支
+
+V1.1.2版本以下
+```bash
+gitee://Token@gitee.com/jadehh_743/TVSpider/js/open_config.json
+```
+> 配置信息见js分支分支
+
+
+## config文件生成
+```bash
+python build.py --aliToken aliToken
+```
+> Token如果失效,需要重新获取阿里Token 
+>
+> [阿里Token获取](https://alist.nn.ci/zh/guide/drivers/aliyundrive.html)
+>
+> nodejs 部分只生成代码，需要手动build，区分18+
+
+## 直播源生成
+> 见[jadehh/LiveSpider](https://github.com/jadehh/LiveSpider)
+
+
+## 遇到的问题
+* 玩偶姐姐播放不了,需要切换VPN节点
+* m3u8遇到跨域的问题可以尝试使用代理来进行加载，如果没有跨域使用代理会引起死循环
+* 虎牙弹幕功能无法实现,现在并不支持WebSocket来监听弹幕
+* SP360启用嗅探解析,CatVodOpen目前还不支持嗅探
+* CatVodOpen Windows无法预览Jable和Doll图片,需要手动开启代理加载。
+* TV影视暂不不支持哔哩哔哩DASH文件播放
+* 老版本的CatVodOpen cfg参数类型为:string,TV参数类型为[object],所有需要区分,初始化的时候还是用this.cfgObj
+
+## 特别说明
+* 近期CatVodOpen更新移除了quickjs,导致无法使用,请尝试使用旧版本
+* 或切换至nodejs目录下编译生成dist目录，dist目录发布到dist分支下
+* main分支用于代码测试(不包含任何配置信息)，js分支发布支持quickjs爬虫配置信息，dist分支发布支持nodejs爬虫配置信息
+* 所有的配置信息都通过Github Actions发布，通过创建tag来生成新的配置信息并自动发布
+* fork仓库时去掉仅复制main分支的&#x2714;，这样就可以fork所有的分支了
+  
+## ✨ Star 数
+
+[![Star History Chart](https://api.star-history.com/svg?repos=jadehh/TVSpider&type=Date)](https://star-history.com/#jadehh/TVSpider&Date)
+---
+
+## 飞机群
+
+加入我们吧
+
+<img src="./assets/image.png" alt="telegram" width="256" height="256" align="left" /> 
+<br><br><br><br><br><br><br><br><br><br>
+
+----
+
+## 赞助
+
+如果觉得此项目有用，可以考虑赞助我喝杯咖啡，感谢star❤
+
+<img src="./resources/wechat.jpg" alt="微信" width="256" height="256" align="left" />
---
+++ js/testSpider.js
@@ -0,0 +1,133 @@
+import {__jsEvalReturn} from '../js/dubo.js';
+
+import * as Utils from "../lib/utils.js";
+
+
+let spider = __jsEvalReturn();
+
+async function testPlay(vodDetail) {
+    if (vodDetail.list && vodDetail.list.length > 0) {
+        const pFlag = vodDetail.list[0].vod_play_from.split('$$$');
+        const pUrls = vodDetail.list[0].vod_play_url.split('$$$');
+        if (pFlag.length > 0 && pUrls.length > 0) {
+            for (const i in pFlag) {
+                // console.debug(i)
+                let flag = pFlag[i];
+                let urls = pUrls[i].split('#');
+                // console.debug(flag, urls)
+                for (const j in urls) {
+                    var name = urls[j].split('$')[0];
+                    var url = urls[j].split('$')[1];
+                    console.debug(flag + " | " + name + " | " + url);
+                    var playUrl = await spider.play(flag, url, []);
+                    console.debug('playURL: ' + playUrl);
+                }
+            }
+        }
+    }
+}
+
+async function testMusicPlay(vodDetail) {
+    if (vodDetail.list && vodDetail.list.length > 0) {
+        const pFlag = vodDetail.list[0].volumes.split('$$$');
+        const pUrls = vodDetail.list[0].urls.split('$$$');
+        if (pFlag.length > 0 && pUrls.length > 0) {
+            for (const i in pFlag) {
+                // console.debug(i)
+                let flag = pFlag[i];
+                let urls = pUrls[i].split('#');
+                // console.debug(flag, urls)
+                for (const j in urls) {
+                    var name = urls[j].split('$')[0];
+                    var url = urls[j].split('$')[1];
+                    console.debug(flag + " | " + name + " | " + url);
+                    var playUrl = await spider.play(flag, url, []);
+                    console.debug('playURL: ' + playUrl);
+                }
+                break
+            }
+        }
+    }
+}
+
+async function test() {
+    let siteKey = 'jianpian';
+    let siteType = 3;
+    await spider.init({
+        skey: siteKey, stype: siteType, ext: {
+            "aliToken": "a0debd28d10e4aa5a431e7de586e6e42",
+            "box": "TV",
+            "code": "1",
+            "from": "star",
+            "danmu": true,
+            "cookie": "buvid3=02675249-8ED3-C418-87F5-59E18316459714816infoc; b_nut=1704421014; _uuid=5D435F74-F574-D9AB-62C1-B9294DE465D913102infoc; buvid_fp=e8c5650c749398e9b5cad3f3ddb5081e; buvid4=007E85D1-52C1-7E6E-07CF-837FFBC9349516677-024010502-J5vTDSZDCw4fNnXRejbSVg%3D%3D; rpdid=|()kYJmulRu0J'u~|RRJl)JR; PVID=1; SESSDATA=3be091d3%2C1720332009%2C699ed%2A11CjAcCdwXG5kY1umhCOpQHOn_WP7L9xFBfWO7KKd4BPweodpR6VyIfeNyPiRmkr5jCqsSVjg0R0dZOVVHRUo3RnhPRTZFc3JPbGdiUjFCdHpiRDhiTkticmdKTjVyS1VhbDdvNjFMSDJlbUJydUlRdjFUNGFBNkJlV2ZTa0N1Q1BEVi1QYTQzTUh3IIEC; bili_jct=b0ee7b5d3f27df893545d811d95506d4; DedeUserID=78014638; DedeUserID__ckMd5=4c8c5d65065e468a; enable_web_push=DISABLE; header_theme_version=CLOSE; home_feed_column=5; CURRENT_BLACKGAP=0; CURRENT_FNVAL=4048; b_lsid=75E916AA_18EA1A8D995; bsource=search_baidu; FEED_LIVE_VERSION=V_HEADER_LIVE_NO_POP; browser_resolution=1507-691; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTIzNjk5MTMsImlhdCI6MTcxMjExMDY1MywicGx0IjotMX0.8zQW_fNTCSBlK_JkHnzu3gDw62wuTK1qgKcbGec3swM; bili_ticket_expires=171236985",
+            "quarkCookie":"__puus=4315f3066f599ce7233f88d8c313a030AAQ/jijbnct663MvWewmjAye1IUH1d9WVf3hcrcIIOEZvWcl4jsz4IUe4nBqByH9+Oc6JSgdx08r93auaJcX5OHH3X8MozlbCX3f7EQ0/UmJwlZiQsz4UKCA++VixQLNd71zoRJ+VvHmYFkJxfnnveJ3nWSWt3HVbUJbAjsWhKR0UrI72TA33ocAbXvyF0XXFVe8lJI4j4DMK6HixNIf1CX2; CwsSessionId=4ebf55ce-02e3-469c-b7fc-59eb327be82c; kkpcwpea=a=a&uc_param_str=einibicppfmivefrlantcunwsssvjbktchnnsnddds&instance=kkpcwp&pf=145&self_service=true&wxUid=AAR33S5CGi7X5FDtOxbk2juf&plain_utdid=Zk29NiM5i2QDAKrmAFKxjrco&system_ver=Darwin_13.6.6&channel_no=pckk%40clouddrive_share_ch&ve=3.2.7&sv=release; _UP_D_=pc; __kp=872521a0-1743-11ef-8758-49b470509a34; __kps=AAR33S5CGi7X5FDtOxbk2juf; __ktd=v/InsJyWIBLv++dfPEXCtA==; __pus=46820c01a28fc1296d407f0bf5cff035AASGf9nOKeDv5D/U50Qsjq6L5IUllrXEQOlPdpQ2S6JymmRlRK1fmlpdAQRkZ4zTHX3i0LGLYxuSwxai4pJtx/pU; __uid=AAR33S5CGi7X5FDtOxbk2juf; _UP_A4A_11_=wb964143699847c8862df3296d502423"
+        }
+    });
+
+    let classes = JSON.parse(await spider.home(true));
+    console.debug(JSON.stringify(classes))
+
+    
+    //测试首页列表
+    let homeVod = JSON.parse(await spider.homeVod())
+    console.debug(JSON.stringify(homeVod));
+
+    
+    // // 测试详情
+    // let detail1 = JSON.parse(await spider.detail("562381"))
+    // await testPlay(detail1)
+
+    /***
+     * 
+     * "HD粤语中英双字$ftp://a.gbl.114s.com:20320/9607/潜行-2023_HD粤语中英双字.mp4#HD国语中英双字$ftp://a.gbl.114s.com:20320/8224/潜行-2023_HD国语中英双字.mp4#BD国粤双语中字$ftp://a.gbl.114s.com:20320/1413/潜行-2023_BD国粤双语中字.mp4"
+     */
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+    // 测试搜索
+    // let search_page = JSON.parse(await spider.search("庆余年", false, 1))
+    // console.debug(JSON.stringify(search_page))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // 测试详情
+    // if (search_page.list && search_page.list.length > 0) {
+    //     for (const k in search_page.list) {
+    //         // console.debug(k)
+    //         if (k >= 1) break;
+    //         let obj = search_page.list[k]
+    //         let spVid = search_page.list[k].vod_id
+    //         console.debug("===", spVid)
+    //         var detail = JSON.parse(await spider.detail(spVid || search_page.list[k].vod_id));
+
+    //         await testPlay(detail);
+    //     }
+    // }
+}
+
+export {test};
